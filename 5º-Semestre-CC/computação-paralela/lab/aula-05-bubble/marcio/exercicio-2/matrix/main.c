3e2125e7d689639974d2455521375d57677763ce
luiznux <luiztagli@hotmail.com>
2020-04-19 17:43:02 -0300

    update file

--- 5º-Semestre-CC/computação-paralela/lab/aula-05-bubble/marcio/exercicio-2/matrix/main.c
+++ 5º-Semestre-CC/computação-paralela/lab/aula-05-bubble/marcio/exercicio-2/matrix/main.c
@@ -4,19 +4,20 @@
 
 
 
-const int TAM = 1000;
+const int TAM = 100;
 
 int main(){
 
     /* crio e populo a matriz com numeros aleatorios */
+
+
    int  **matrix = calloc_matrix(TAM, TAM);
    fills_rand_matrix(matrix, TAM, TAM, 20);
-
+   int value = 7;
 
 /*----------------------- sem paralelização -----------------*/
 
    clock_t inicio_ruim = clock();
-   int value = 7;
    int count = 0;
    for(int i = 0; i < TAM; i++){
        for(int j = 0; j < TAM; j++){
@@ -43,10 +44,11 @@ int main(){
    int count4 = 0;
    int count5 = 0;
 
-#pragma omp parallel sections
+
+#pragma omp parallel sections num_threads(4)
    {
        /*quadrante superior esquerdo*/
-#pragma omp parallel section
+#pragma omp section
        {
            inicio_bom_1 = clock();
            for(int i = 0; i < TAM/2; i++){
@@ -58,36 +60,36 @@ int main(){
        }
 
        /*quadrante superior direito*/
-#pragma omp parallel section
+#pragma omp section
        {
            inicio_bom_2 = clock();
            for(int i = 0; i < TAM/2; i++){
                for(int j = TAM/2; j < TAM; j++){
-                   if(matrix[i][j] > value) count2++;
+                   if(matrix[i][j] > value) count3++;
                }
            }
                fim_bom_2 = clock();
        }
 
        /* quadrante inferiro esquerdo */
-       #pragma omp parallel section
+#pragma omp section
        {
            inicio_bom_3 = clock();
            for(int i = TAM/2; i < TAM; i++){
                for(int j = 0; j < TAM/2; j++){
-                   if(matrix[i][j] > value) count2++;
+                   if(matrix[i][j] > value) count4++;
                }
            }
            fim_bom_3 = clock();
        }
 
        /* quadrante inferior direito */
-#pragma omp parallel section
+#pragma omp section
        {
            inicio_bom_4 = clock();
            for(int i = TAM/2; i < TAM; i++){
                for(int j = TAM/2; j < TAM; j++){
-                   if(matrix[i][j] > value) count2++;
+                   if(matrix[i][j] > value) count5++;
                }
            }
            fim_bom_4  = clock();
@@ -107,7 +109,7 @@ int main(){
    soma_dos_bons += fim_bom_3 - inicio_bom_3 ;
    soma_dos_bons += fim_bom_4 - inicio_bom_4 ;
    const double tempo_bom_total = (double)(soma_dos_bons) / CLOCKS_PER_SEC;
-   printf("O tempo c/ paralelizaçãoé %lf segundos \n",tempo_bom_total);
+   printf("O tempo c/ paralelização é %lf segundos \n",tempo_bom_total);
 
     return 0;
 }
